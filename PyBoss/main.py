e5a353c0169bc3f8c6b0ff9e14591f7833f4b9c8
Kip Madden <kipmadden@cosapient.com>
2019-12-20 17:04:54 -0600

    Initial code block

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 # Create a file path for the election_data csv file
-csvpath = os.path.join('Resources', 'election_data.csv')
+csvpath = os.path.join('Resources', 'employee_data.csv')
 
 # Read in the csv file
 with open(csvpath, newline='') as csvfile:
@@ -11,25 +11,41 @@ with open(csvpath, newline='') as csvfile:
     # Using the csvfile object create csvreader variable identifying the delimiter as a comma
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Set variable to capture two columns of data VoterID and Candidate name
-    idList = []
+    # Set variable to capture two columns of data EmpID and Candidate name
+    employeeList = []
     canList = []
     canDict = {}
+    # Set variable for output file path
+    output_file = os.path.join('Resources','employee_data_new.csv')
     
-        # Read each row of data and capture VoterID and Candidate name into a dictionary idList
+    # Make a list of dictionaries 
     for row in csvreader:
-        # Check if the line read in is the header - if it is don't add it to the idList
-        if row[0] != "Voter ID":
-               
-            idList.append(row[0])
-            canList.append(row[2])
 
-# Check if there is more than one occurance of the VoterID (this would be voter fraud) 
-if len(idList) != len(set(idList)):
+        # Check if the line read in is the header - if it is don't add it to the idDict
+        if row[0] != "Emp ID":
+            empId,name,dob,ssn,state = row.strip().split(',')
+            data = values.split(',')
+            # Print results to a file named pyPoll_results.csv
+
+
+
+            #  Open the output file path using a context manager
+            with open(output_file, "w", newline="") as datafile:
+                # pass the datafile object to the writer
+                writer = csv.writer(datafile)
+
+                # Write the result as a separate line from the list results (each element is a string) 
+                # so we need to use a nested list to write each string as a separate line because the
+                # .writerows method takes an iterable and uses each element of that iterable for each column
+                for result in results:
+                    writer.writerows([[result]])
+
+# Check if there is more than one occurance of the EmpID (this would be voter fraud) 
+if len(employeeList) != len(set(employeeList)):
     print(f'Looks like there is voter fraud!')
 else:
-    # Determine the total votes by looking at the length of the list idList
-    totalVotes = len(idList)
+    # Determine the total votes by looking at the length of the list employeeList
+    totalVotes = len(employeeList)
     # Store the unique list of candidate names by taking a set of canList list
     candidatesVoted = (list(set(canList)))
     
@@ -66,18 +82,3 @@ results.append(f'Winner: {winner}')
 for result in results:
     print(result)
 
-# Print results to a file named pyPoll_results.csv
-
-# Set variable for output file path
-output_file = os.path.join('Resources','pyPoll_results.csv')
-
-#  Open the output file path using a context manager
-with open(output_file, "w", newline="") as datafile:
-    # pass the datafile object to the writer
-    writer = csv.writer(datafile)
-
-    # Write the result as a separate line from the list results (each element is a string) 
-    # so we need to use a nested list to write each string as a separate line because the
-    # .writerows method takes an iterable and uses each element of that iterable for each column
-    for result in results:
-        writer.writerows([[result]])
