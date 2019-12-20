a38112fd657c41d39108b0b73bb592175349d3f0
Kip Madden <kipmadden@cosapient.com>
2019-12-20 03:03:04 -0600

    initial copy code of pyBank

---
+++ PyPoll/main.py
@@ -0,0 +1,63 @@
+# import the os, csv and statistics modules
+import os
+import csv
+from statistics import mean as avg
+
+# Create a file path for the election_data csv file
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+# Read in the csv file
+with open(csvpath, newline='') as csvfile:
+
+    # Using the csvfile object create csvreader variable identifying the delimiter as a comma
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Set variable to capture header if there is one, and a list for month data and P&L data
+    headerval = []
+    months = []
+    pAndL = []
+        # Read each row of data after the header
+    for row in csvreader:
+        # Check if the first row is a header with the label "Date" in the first position
+        if row[0] == "Date":
+            # Store the header in a list variable
+            headerval = row
+            # if not the header row then append the values to the appropriate list
+        else:
+            months.append(row[0])
+            pAndL.append(int(row[1]))
+
+# Save the results as a list of strings so we can both print to the terminal and to a file
+# without having to write out all the lines explicitly twice
+
+# create an empty list to store output f strings for results
+results = []
+
+# Append each result string to the results list
+results.append(f'Financial Analysis')
+results.append(f'------------------------------')
+results.append(f'Total Months: {len(months)}')
+results.append(f'Total: ${(sum(pAndL)):,}')
+results.append(f'Average Change: ${round(avg(pAndL),2):,}')
+results.append(f'Greatest Increase in Profits: {months[(pAndL.index(max(pAndL)))]} ${max(pAndL):,}')
+results.append(f'Greatest Decrease in Profits: {months[(pAndL.index(min(pAndL)))]} ${min(pAndL):,}')
+
+# Iterate through the list of strings and print each one to the terminal
+for result in results:
+    print(result)
+
+# Print results to a file named pybank_results.csv
+
+# Set variable for output file path
+output_file = os.path.join('Resources','pybank_results.csv')
+
+#  Open the output file path using a context manager
+with open(output_file, "w", newline="") as datafile:
+    # pass the datafile object to the writer
+    writer = csv.writer(datafile)
+
+    # Write the result as a separate line from the list results (each element is a string) 
+    # so we need to use a nested list to write each string as a separate line because the
+    # .writerows method takes an iterable and uses each element of that iterable for each column
+    for result in results:
+        writer.writerows([[result]])
