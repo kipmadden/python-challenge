78813c01c36683dda9675ea9379742ee08ddd3e0
Kip Madden <kipmadden@cosapient.com>
2019-12-20 15:00:52 -0600

    full changes made after using Jupyter notebook for testing

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,6 @@
 # import the os, csv and statistics modules
 import os
 import csv
-from statistics import mean as avg
 
 # Create a file path for the election_data csv file
 csvpath = os.path.join('Resources', 'election_data.csv')
@@ -12,44 +11,65 @@ with open(csvpath, newline='') as csvfile:
     # Using the csvfile object create csvreader variable identifying the delimiter as a comma
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Set variable to capture header if there is one, and a list for month data and P&L data
-    headerval = []
-    months = []
-    pAndL = []
-        # Read each row of data after the header
+    # Set variable to capture two columns of data VoterID and Candidate name
+    idList = []
+    canList = []
+    canDict = {}
+    
+        # Read each row of data and capture VoterID and Candidate name into a dictionary idDict
     for row in csvreader:
-        # Check if the first row is a header with the label "Date" in the first position
-        if row[0] == "Date":
-            # Store the header in a list variable
-            headerval = row
-            # if not the header row then append the values to the appropriate list
-        else:
-            months.append(row[0])
-            pAndL.append(int(row[1]))
+        # Check if the line read in is the header - if it is don't add it to the idDict
+        if row[0] != "Voter ID":
+               
+            idList.append(row[0])
+            canList.append(row[2])
 
-# Save the results as a list of strings so we can both print to the terminal and to a file
-# without having to write out all the lines explicitly twice
+# Check if there is more than one occurance of the VoterID (this would be voter fraud) 
+if len(idList) != len(set(idList)):
+    print(f'Looks like there is voter fraud!')
+else:
+    # Determine the total votes by looking at the length of the list idList
+    totalVotes = len(idList)
+    # Store the unique list of candidate names by taking a set of canList list
+    candidatesVoted = (list(set(canList)))
+    
+    winner = ''
+    winvotes = 0
+    
+    # Store the number of votes each candidate received and the percentage of the total votes as values in a dictionary
+    # with the candidate name as the key
+    for cand in candidatesVoted:
+        canDict[cand] = [canList.count(cand),round(canList.count(cand)/totalVotes*100,3)]
+        # Check if the number of votes the current candidate has is greater than the current winvotes variable
+        # If it is then replace the value of winvotes with the new higher count and set the winner variable to the
+        # name of the current candidate
+        if int(canList.count(cand)) > winvotes:
+            winvotes = int(canList.count(cand))
+            winner = cand
 
 # create an empty list to store output f strings for results
 results = []
 
 # Append each result string to the results list
-results.append(f'Financial Analysis')
+results.append(f'Election Results')
 results.append(f'------------------------------')
-results.append(f'Total Months: {len(months)}')
-results.append(f'Total: ${(sum(pAndL)):,}')
-results.append(f'Average Change: ${round(avg(pAndL),2):,}')
-results.append(f'Greatest Increase in Profits: {months[(pAndL.index(max(pAndL)))]} ${max(pAndL):,}')
-results.append(f'Greatest Decrease in Profits: {months[(pAndL.index(min(pAndL)))]} ${min(pAndL):,}')
+results.append(f'Total Votes: {totalVotes:,}')
+results.append(f'------------------------------')
+# Iterate through the canDict dictionary and print out the results to the screen
+for candidate in canDict.keys():
+    results.append(f'{candidate}: {canDict[candidate][1]}% {canDict[candidate][0]:,}')
+results.append(f'------------------------------')
+results.append(f'Winner: {winner}')
+
 
 # Iterate through the list of strings and print each one to the terminal
 for result in results:
     print(result)
 
-# Print results to a file named pybank_results.csv
+# Print results to a file named pyPoll_results.csv
 
 # Set variable for output file path
-output_file = os.path.join('Resources','pybank_results.csv')
+output_file = os.path.join('Resources','pyPoll_results.csv')
 
 #  Open the output file path using a context manager
 with open(output_file, "w", newline="") as datafile:
