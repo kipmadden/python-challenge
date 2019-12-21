618db9b226370a996a9c7b6558e8f6adb7d2e7d9
Kip Madden <kipmadden@cosapient.com>
2019-12-21 15:34:13 -0600

    Fixed the P&L difference calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 # import the os, csv and statistics modules
 import os
 import csv
-from statistics import mean as avg
+from statistics import mean
 
 # Create a file path for the budget_data csv file
 csvpath = os.path.join('Resources', 'budget_data.csv')
@@ -16,7 +16,9 @@ with open(csvpath, newline='') as csvfile:
     headerval = []
     months = []
     pAndL = []
-
+    pAndLDiffList = []
+    priorVal = 0
+    firstVal = True
     # Read each row of data after the header
     for row in csvreader:
         # Check if the first row is a header with the label "Date" in the first position
@@ -27,7 +29,22 @@ with open(csvpath, newline='') as csvfile:
         else:
             months.append(row[0])
             pAndL.append(int(row[1]))
+            # Calculate the difference between the current profit and loss and the prior profit and loss
+            # Don't caluclate a difference if we are on the first value
+            if firstVal:
+                # Set the priorVal to the current P&L value for use in calculating the P&L difference for the second P&L value in the data
+                priorVal = int(row[1])
+                # We've dealt with the first P&L difference value so set the firstVal state variable to False
+                firstVal = False
+            else:
+                # Caluculate the first P&L difference
+                pAndLDiff = int(row[1]) - int(priorVal)
+                # Add the P&L difference to the pAndLDiffList
+                pAndLDiffList.append(int(pAndLDiff))
+                # Increment the priorVal to the current P&L value for use in the next iteration of the P&L difference calculation
+                priorVal = int(row[1])
 
+    
 # Save the results as a list of strings so we can both print to the terminal and to a file
 # without having to write out all the lines explicitly twice
 
@@ -39,9 +56,9 @@ results.append(f'Financial Analysis')
 results.append(f'------------------------------')
 results.append(f'Total Months: {len(months)}')
 results.append(f'Total: ${(sum(pAndL)):,}')
-results.append(f'Average Change: ${round(avg(pAndL),2):,}')
-results.append(f'Greatest Increase in Profits: {months[(pAndL.index(max(pAndL)))]} ${max(pAndL):,}')
-results.append(f'Greatest Decrease in Profits: {months[(pAndL.index(min(pAndL)))]} ${min(pAndL):,}')
+results.append(f'Average Change: ${round(mean(pAndLDiffList),2):,}')
+results.append(f'Greatest Increase in Profits: {months[(pAndLDiffList.index(max(pAndLDiffList)))+1]} ${max(pAndLDiffList):,}')
+results.append(f'Greatest Decrease in Profits: {months[(pAndLDiffList.index(min(pAndLDiffList)))+1]} ${min(pAndLDiffList):,}')
 
 # Iterate through the list of strings and print each one to the terminal
 for result in results:
