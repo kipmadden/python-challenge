c138dd4e8bcd411eeaf6cd397f10e9e0c9dcf214
Kip Madden <kipmadden@cosapient.com>
2019-12-21 02:30:44 -0600

    commenting finished - final commit

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -32,34 +32,44 @@ with open(csvpath, newline='') as csvfile:
     # Make a list of dictionaries 
     for row in csvreader:
 
-        # Check if the line read in is the header - if it is don't add it to the idDict
+        # Check if the row read in is the header - if it isn't process the row
         if row[0] != "Emp ID":
+            # Assign the strings in the list to variables
             empId,name,dob,ssn,state = [elem for elem in row]
-            firstName,lastName = str(name).split(' ')
-            year,month,day = str(dob).split('-')
+            # Split the name field into firstName and lastName 
+            firstName,lastName = name.split(' ')
+            # Split the dob field into year, month and day
+            year,month,day = dob.split('-')
+            # format the dobNew using f-string
             dobNew = f'{month}/{day}/{year}'
-            ssn1,ssn2,ssn3 = str(ssn).split('-')
+            # Split the SSN by the '-' into three parts
+            ssn1,ssn2,ssn3 = ssn.split('-')
+            # Obfuscate the first 5 digits of the SSN and only show the last 4 digits
             ssnNew = f'***-**-{ssn3}'
-            stateNew = us_state_abbrev[str(state)]
+            # Use us_state_abbrev dict to transform the fully spelled state name into 2 letter code
+            stateNew = us_state_abbrev[state]
+            # Form a new list of strings and append it to the list of lists employeeList
             newRow = [empId,firstName,lastName,dobNew,ssnNew,stateNew]
             employeeList.append(newRow)
+        # Check if the firstRow variable is True - meaning we haven't written the header line
         if firstRow:
+            # initialize the employeeList with the newHeader list and set firstRow variable to False
             employeeList.append(newHeader)
             firstRow = False
-        # Print results to a file named pyPoll_results.csv
-        #  Open the output file path using a context manager
+    # Print results to a file named employee_data_new.csv
+    #  Open the output file path using a context manager
     with open(output_file, "w", newline="") as datafile:
         # pass the datafile object to the writer
         writer = csv.writer(datafile)
 
-        # Write the result as a separate line from the list results (each element is a string) 
-        # so we need to use a nested list to write each string as a separate line because the
-        # .writerows method takes an iterable and uses each element of that iterable for each column
+        # The .writerows method takes an iterable and uses each element of that iterable for each column
+        # employeeList is a list of lists, therefore result is a list (iterable)
         for result in employeeList:
             writer.writerows([result])
 
 
 # Iterate through the list of strings and print each one to the terminal
-for result in employeeList:
-    print(result)
+#for result in employeeList:
+#    print(result)
+
 
