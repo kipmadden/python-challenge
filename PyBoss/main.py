1b4630145362e5338679b1af9fbde8a69fbb0887
Kip Madden <kipmadden@cosapient.com>
2019-12-20 22:28:49 -0600

    modified string handling

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -34,16 +34,18 @@ with open(csvpath, newline='') as csvfile:
 
         # Check if the line read in is the header - if it is don't add it to the idDict
         if row[0] != "Emp ID":
-            empId,name,dob,ssn,state = row.strip().split(',')
-            firstName,lastName = name.split(' ')
-            year,month,day = dob.split('-')
-            dobNew = f('{month}/{day}/{year}')
-            ssn1,ssn2,ssn3 = ssn.split('-')
-            ssnNew = f('***-**-{ssn3}')
-            stateNew = us_state_abbrev[state]
+            empId,name,dob,ssn,state = [elem.strip().split(';') for elem in row]
+            print(empId)
+            firstName,lastName = str(name).split(' ')
+            year,month,day = str(dob).split('-')
+            dobNew = str(f'{month}/{day}/{year}')
+            ssn1,ssn2,ssn3 = str(ssn).split('-')
+            ssnNew = f'***-**-{ssn3}'
+            stateNew = us_state_abbrev[[state]]
             newRow = list(empId,firstName,lastName,dobNew,ssnNew,stateNew)
+            employeeList.append(newRow)
         else:
-
+            employeeList.append(newHeader)
         # Print results to a file named pyPoll_results.csv
         #  Open the output file path using a context manager
         with open(output_file, "w", newline="") as datafile:
@@ -53,48 +55,11 @@ with open(csvpath, newline='') as csvfile:
             # Write the result as a separate line from the list results (each element is a string) 
             # so we need to use a nested list to write each string as a separate line because the
             # .writerows method takes an iterable and uses each element of that iterable for each column
-            for result in results:
-                writer.writerows([[result]])
-
-# Check if there is more than one occurance of the EmpID (this would be voter fraud) 
-if len(employeeList) != len(set(employeeList)):
-    print(f'Looks like there is voter fraud!')
-else:
-    # Determine the total votes by looking at the length of the list employeeList
-    totalVotes = len(employeeList)
-    # Store the unique list of candidate names by taking a set of canList list
-    candidatesVoted = (list(set(canList)))
-    
-    winner = ''
-    winvotes = 0
-    
-    # Store the number of votes each candidate received and the percentage of the total votes as values in a dictionary
-    # with the candidate name as the key
-    for cand in candidatesVoted:
-        canDict[cand] = [canList.count(cand),round(canList.count(cand)/totalVotes*100,3)]
-        # Check if the number of votes the current candidate has is greater than the current winvotes variable
-        # If it is then replace the value of winvotes with the new higher count and set the winner variable to the
-        # name of the current candidate
-        if int(canList.count(cand)) > winvotes:
-            winvotes = int(canList.count(cand))
-            winner = cand
-
-# create an empty list to store output f strings for results
-results = []
-
-# Append each result string to the results list
-results.append(f'Election Results')
-results.append(f'------------------------------')
-results.append(f'Total Votes: {totalVotes:,}')
-results.append(f'------------------------------')
-# Iterate through the canDict dictionary and print out the results to the screen
-for candidate in canDict.keys():
-    results.append(f'{candidate}: {canDict[candidate][1]}% {canDict[candidate][0]:,}')
-results.append(f'------------------------------')
-results.append(f'Winner: {winner}')
+            for result in employeeList:
+                writer.writerows([[employeeList]])
 
 
 # Iterate through the list of strings and print each one to the terminal
-for result in results:
+for result in employeeList:
     print(result)
 
