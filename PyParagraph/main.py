1633cce6f3baa2ede7794f9b4f0999b3e90aeb2d
Kip Madden <kipmadden@cosapient.com>
2019-12-21 14:18:50 -0600

    Added user input mode and Regex analysis

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -3,9 +3,20 @@ import os
 import csv
 import re
 
-# Create a file path for the paragraph text file
-csvpath = os.path.join('raw_data', 'paragraph_1.txt')
+# Ask for user input to run file 1, file 2
+userInput = input(f'Analyze (1) paragraph_1.txt (2) paragraph_2.txt')
 
+# Parse userInput and create filepath accordingly
+if userInput == 1:
+    csvpath = os.path.join('raw_data', 'paragraph_1.txt')
+elif userInput == 2:
+    csvpath = os.path.join('raw_data', 'paragraph_2.txt')
+else:
+    print(f'You entered an invalid entry so we will just pick paragraph_1.txt')  
+    csvpath = os.path.join('raw_data', 'paragraph_1.txt')
+
+
+directoryPath,filePath = csvpath.split('\\')
 # Read in the csv file
 with open(csvpath, newline='') as csvfile:
 
@@ -18,6 +29,10 @@ with open(csvpath, newline='') as csvfile:
     # Set variable for output file path
     output_file = os.path.join('raw_data','paragraphOutput.txt')
     
+    #Print the first line to the terminal
+    print()
+    print(f'Paragraph Analysis for the file: {filePath} in the folder: {directoryPath}')
+    print('---------------------------------------------------------------------------')
     # iterate throught the list 
     for row in csvreader:
         string = ' '.join(row)
@@ -25,19 +40,35 @@ with open(csvpath, newline='') as csvfile:
 
     # Count the number of words by splitting on spaces
     numWords = len(giantString.split()) 
-    print(f'Number of words: {numWords}')
+    print(f'Approximate Word Count separating on spaces: {numWords}')
+
+    # Alternate method using regex
+    wordRegex = r'\w+'
+    wordList = re.findall(wordRegex,giantString)
+    numRegexWords = len(wordList)
+    print(f'Approximate Regex "\w+" Word Count: {numRegexWords}')
 
     # Count sentences by enumerating period characters
     sentenceSplit = re.split("(?<=[.!?]) +", giantString)
     numSentence = len(sentenceSplit)
-    print(f'Sentence List: {sentenceSplit}')
-    print(f'Approximate Sentence Count:{numSentence}')
+    #print(f'Sentence List: {sentenceSplit}')
+    print(f'Approximate Sentence Count: {numSentence}')
 
-
+    # Count the Average Letter Count by only considering characters A-Z and a-z (no letters from other languages)
+    # and dividing by the Regex Word count
+    letterRegex = r'[A-Za-z]'
+    letterRegexList = re.findall(letterRegex,giantString)
+    letterRegexCount = len(letterRegexList)
+    letterCountRegexAvg = round(letterRegexCount/numRegexWords,2)
+    print(f'Total Letter Count "[A-Z,a-z]: {letterRegexCount}')
+    print(f'Average Letter Count using Regex : {letterCountRegexAvg}')
 
     # Calculate the average sentence length by dividing the number of Words by the number of sentences
-    sentenceLength = numWords / numSentence
+    sentenceLength = numRegexWords / numSentence
     print(f'Average Sentence Length: {sentenceLength}')
+
+
+    #print(wordList)
         # Check if the row read in is the header - if it isn't process the row
         # if row[0] != "Emp ID":
         #     # Assign the strings in the list to variables
